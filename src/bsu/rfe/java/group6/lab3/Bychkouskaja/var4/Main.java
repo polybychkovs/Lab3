bb8446f7a3d8e6a33d05497951207bc6acdb23b9
BychkovskajaPolina <polina14ght@mail.ru>
2021-12-09 20:01:09 +0300

    created main

--- src/bsu/rfe/java/group6/lab3/Bychkouskaja/var4/Main.java
+++ src/bsu/rfe/java/group6/lab3/Bychkouskaja/var4/Main.java
@@ -1,8 +1,29 @@
 package bsu.rfe.java.group6.lab3.Bychkouskaja.var4;
 
+import javax.swing.*;
+
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+        if (args.length == 0) {
+            System.out.println("Невозможно табулировать многочлен, для которого не задано ни одного коэффициента!");
+            System.exit(-1);
+        }
+        Double[] coefficients = new Double[args.length];
+        int i = 0;
+        try {
+            for (String arg : args) {
+                coefficients[i++] = Double.parseDouble(arg);
+            }
+        } catch (NumberFormatException ex) {
+            System.out.println("Ошибка преобразования строки '" +
+                    args[i] + "' в число типа Double");
+            System.exit(-2);
+        }
+        MainFrame frame = new MainFrame(coefficients);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
