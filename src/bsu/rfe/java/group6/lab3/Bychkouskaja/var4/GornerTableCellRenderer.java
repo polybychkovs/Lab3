0ca7b1025820f00c8ff3ba55362de9cc0fbaeae3
BychkovskajaPolina <polina14ght@mail.ru>
2021-12-09 19:57:31 +0300

    created table cell renderer

---
+++ src/bsu/rfe/java/group6/lab3/Bychkouskaja/var4/GornerTableCellRenderer.java
@@ -0,0 +1,52 @@
+package bsu.rfe.java.group6.lab3.Bychkouskaja.var4;
+
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class GornerTableCellRenderer implements TableCellRenderer{
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+
+    private String needle = null;
+    private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
+    //конструктор класса-визуализатора
+    public GornerTableCellRenderer() {
+        formatter.setMaximumFractionDigits(5);
+        formatter.setGroupingUsed(false);
+        DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(dottedDouble);
+        panel.add(label);
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    }
+
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                                                   boolean hasFocus, int row, int col) {
+// Преобразовать double в строку с помощью форматировщика
+        String formattedDouble = formatter.format(value);
+// Установить текст надписи равным строковому представлению числа
+        label.setText(formattedDouble);
+        if (col==1 && needle != null && needle.equals(formattedDouble)) {
+            panel.setBackground(Color.RED);
+        }
+        else {
+            panel.setBackground(Color.WHITE);
+        }
+
+        return panel;
+    }
+
+    public void setNeedle(String needle) {
+        this.needle = needle;
+    }
+}
+
